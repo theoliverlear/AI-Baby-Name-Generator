227e8e6c63818a21d8e925528439ffdbab6bceb5
theoliverlear <sigwartholiver2@gmail.com>
2024-05-15 21:29:02 -0500

    Add new file GeneratorController.java for managing generator requests

---
+++ src/main/java/org/theoliverlear/controller/GeneratorController.java
@@ -0,0 +1,13 @@
+package org.theoliverlear.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/generator")
+public class GeneratorController {
+    @RequestMapping("/")
+    public String generate() {
+        return "generator";
+    }
+}
