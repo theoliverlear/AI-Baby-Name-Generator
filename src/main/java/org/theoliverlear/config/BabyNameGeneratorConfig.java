7341e54e6f9f43f8373a3e2e4e3aca5cb7f2374e
theoliverlear <sigwartholiver2@gmail.com>
2024-05-16 19:05:29 -0500

    Add contextualizing comments for better readability in BabyNameGeneratorConfig.java

--- src/main/java/org/theoliverlear/config/BabyNameGeneratorConfig.java
+++ src/main/java/org/theoliverlear/config/BabyNameGeneratorConfig.java
@@ -1,8 +1,12 @@
 package org.theoliverlear.config;
-
+//=================================-Imports-==================================
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BabyNameGeneratorConfig {
+    //============================-Variables-=================================
 
+    //===========================-Constructors-===============================
+
+    //==============================-Beans-===================================
 }
