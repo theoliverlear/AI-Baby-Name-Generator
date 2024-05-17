2e95e5eeadd3c3429926e8daa1a7ddc08d6c7b32
theoliverlear <sigwartholiver2@gmail.com>
2024-05-16 19:05:02 -0500

    Add contextualizing comments for better readability in ApiSettings.java

--- src/main/java/org/theoliverlear/model/message/ApiSettings.java
+++ src/main/java/org/theoliverlear/model/message/ApiSettings.java
@@ -1,5 +1,5 @@
 package org.theoliverlear.model.message;
-
+//=================================-Imports-==================================
 import lombok.Getter;
 import lombok.Setter;
 import org.theoliverlear.model.ai.AiModel;
@@ -7,11 +7,14 @@ import org.theoliverlear.model.ai.AiModel;
 @Getter
 @Setter
 public class ApiSettings {
-    public static final ApiSettings DEFAULT = new ApiSettings(AiModel.GPT_4, 100, 0.7f);
+    //============================-Variables-=================================
     private AiModel aiModel;
     private int maxTokens;
     private float temperature;
     private String apiKey;
+    //============================-Constants-=================================
+    public static final ApiSettings DEFAULT = new ApiSettings(AiModel.GPT_4, 100, 0.7f);
+    //===========================-Constructors-===============================
     public ApiSettings(AiModel aiModel, int maxTokens, float temperature) {
         this.aiModel = aiModel;
         this.maxTokens = maxTokens;
