6f176f81259241fd83004b3bb2bdd74ffa011c19
theoliverlear <sigwartholiver2@gmail.com>
2024-05-16 19:21:50 -0500

    Refactor NameListSeperator.java to always split names by comma and trim whitespaces

--- src/main/java/org/theoliverlear/model/name/NameListSeperator.java
+++ src/main/java/org/theoliverlear/model/name/NameListSeperator.java
@@ -1,18 +1,18 @@
 package org.theoliverlear.model.name;
-
+//=================================-Imports-==================================
 import org.theoliverlear.entity.name.Name;
 import org.theoliverlear.entity.name.Names;
 
 public class NameListSeperator {
+    //=============================-Methods-==================================
+
+    //---------------------------Separate-Names-------------------------------
     public static Names seperateNames(String namesString) {
         Names names = new Names();
         String delimiter = ",";
-        if (namesString.contains(" ")) {
-            delimiter = " ";
-        }
         String[] namesArray = namesString.split(delimiter);
         for (String name : namesArray) {
-            names.addName(new Name(name));
+            names.addName(new Name(name.trim()));
         }
         return names;
     }
