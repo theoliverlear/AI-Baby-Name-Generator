c1e696294248edcb2178adc5ed02bb15111d9a29
theoliverlear <sigwartholiver2@gmail.com>
2024-05-16 19:20:12 -0500

    Update Name.java by adding Rating attribute, constructor initializations, and overriding toString method

--- src/main/java/org/theoliverlear/entity/name/Name.java
+++ src/main/java/org/theoliverlear/entity/name/Name.java
@@ -1,16 +1,33 @@
 package org.theoliverlear.entity.name;
-
+//=================================-Imports-==================================
 import lombok.Getter;
 import lombok.Setter;
+import org.theoliverlear.entity.rating.Rating;
 
 @Getter
 @Setter
 public class Name {
-    String name;
+    //============================-Variables-=================================
+    private String name;
+    private Rating rating;
+    //===========================-Constructors-===============================
     public Name() {
         this.name = "";
+        this.rating = Rating.NO_RATING;
     }
     public Name(String name) {
         this.name = name;
+        this.rating = Rating.NO_RATING;
+    }
+    public Name(String name, Rating rating) {
+        this.name = name;
+        this.rating = rating;
+    }
+    //============================-Overrides-=================================
+
+    //------------------------------To-String---------------------------------
+    @Override
+    public String toString() {
+        return this.name;
     }
 }
