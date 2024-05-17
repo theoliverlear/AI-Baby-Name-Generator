e835f7498454eb2fd3e067dccba148e10049f2a5
theoliverlear <sigwartholiver2@gmail.com>
2024-05-16 19:06:39 -0500

    Add contextualizing comments for better readability in BabyNameGeneratorController.java

--- src/main/java/org/theoliverlear/controller/BabyNameGeneratorController.java
+++ src/main/java/org/theoliverlear/controller/BabyNameGeneratorController.java
@@ -1,11 +1,14 @@
 package org.theoliverlear.controller;
 
-
+//=================================-Imports-==================================
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BabyNameGeneratorController {
+    //=============================-Methods-==================================
+
+    //--------------------------------Home------------------------------------
     @RequestMapping("/")
     public String home() {
         return "home";
