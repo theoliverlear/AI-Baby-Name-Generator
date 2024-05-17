d8d8bbed1eea9f1464ac970e77888fe019c3712c
theoliverlear <sigwartholiver2@gmail.com>
2024-05-16 19:03:24 -0500

    Add contextualizing comments for better readability in AIBabyNameGeneratorApplication.java

--- src/main/java/org/theoliverlear/AIBabyNameGeneratorApplication.java
+++ src/main/java/org/theoliverlear/AIBabyNameGeneratorApplication.java
@@ -1,10 +1,11 @@
 package org.theoliverlear;
-
+//=================================-Imports-==================================
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AIBabyNameGeneratorApplication {
+    //===============================-Main-===================================
     public static void main(String[] args) {
         SpringApplication.run(AIBabyNameGeneratorApplication.class, args);
     }
