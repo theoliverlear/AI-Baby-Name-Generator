e9852c5d2dad24ca324c1b47e5cd3e8dc3ff6d16
theoliverlear <sigwartholiver2@gmail.com>
2024-07-08 18:29:09 -0500

    Remove unnecessary method mimicking a hyperlink click in homeScript.ts

--- src/main/resources/static/script/homeScript.ts
+++
@@ -1,5 +0,0 @@
-let generateNowButton: HTMLElement = document.getElementById("generate-now-div");
-function goToGeneratePage(): void {
-    window.location.href = "/generator/";
-}
-generateNowButton.addEventListener("click", goToGeneratePage);
